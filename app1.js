6573a4bf0d1e3bb12d6ff8e3377c0f31d946cb96
maheakdave <93195196+maheakdave@users.noreply.github.com>
2023-12-09 12:22:22 +0530

    Create app1.js

---
+++ app1.js
@@ -0,0 +1,17 @@
+const router = require('./routes/router.js');
+const express = require('express');
+const path = require('path')
+require('dotenv').config();
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/BlockCure');
+app = express();
+const PORT = process.env.PORT;
+
+app.set('view engine','ejs');
+app.set('views',path.resolve('./views'));
+app.use(bodyparser.urlencoded({extended:false}));
+app.use('/',router);
+
+app.listen(PORT,()=>console.log("Server Listening on PORT:"+PORT));
